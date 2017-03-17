f728221316d012227814ae798138f0797ce0a1c1
Vinícius Washington Nichals Filho <Vincius Washington Nichals Filho>
2017-03-16 21:17:33 -0300

    Criadas as activity.

---
+++ AS_Android/app/src/main/java/com/example/nicha/as_android/Consultar_ProdutoActivity.java
@@ -0,0 +1,13 @@
+package com.example.nicha.as_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Consultar_ProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar__produto);
+    }
+}
