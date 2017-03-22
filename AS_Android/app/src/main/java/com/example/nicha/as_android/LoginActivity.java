e6f3357a9c9c53ea189d48c82ab83a68d688fa11
Vinicius Washington Nichals Filho <nichalsf@hotmail.com>
2017-03-22 19:04:16 -0300

    Criada logica de login temporária.

--- AS_Android/app/src/main/java/com/example/nicha/as_android/LoginActivity.java
+++ AS_Android/app/src/main/java/com/example/nicha/as_android/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.nicha.as_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +11,15 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void autenticar(){
+        EditText login = (EditText) findViewById(R.id.txtUsuario);
+        EditText senha = (EditText) findViewById(R.id.txtSenha);
+
+        if (login.getText().equals("1") && senha.getText().equals("admin"))
+        {
+            Intent intent =  new Intent(getApplicationContext(), PaginaPrincipalActivity.class);
+        }
     }
 }
