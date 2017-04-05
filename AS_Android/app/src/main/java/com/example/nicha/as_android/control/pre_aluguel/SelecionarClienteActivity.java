d7b1400a13d58e847c4d76d49f48fc98787e5c8f
Vinicius Washington Nichals Filho <nichalsf@hotmail.com>
2017-04-05 13:10:40 -0300

    .

--- AS_Android/app/src/main/java/com/example/nicha/as_android/control/pre_aluguel/SelecionarClienteActivity.java
+++ AS_Android/app/src/main/java/com/example/nicha/as_android/control/pre_aluguel/SelecionarClienteActivity.java
@@ -26,10 +26,9 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SelecionarClienteActivity extends Activity
-{
+public class SelecionarClienteActivity extends Activity {
 
-    List<Cliente> listaCliente = new ArrayList<Cliente>();
+    List<Cliente> listaCliente;
     ListView listViewCliente;
     ClienteDTO clienteDto;
     ArrayAdapter<Cliente> clienteAdapter;
@@ -37,111 +36,71 @@ public class SelecionarClienteActivity extends Activity
     Cliente clienteSelecionado;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_aluguel_selecionar_cliente);
+        listaCliente = new ArrayList<Cliente>();
         listViewCliente = (ListView) findViewById(R.id.listCliente);
         txtPesquisa = (EditText) findViewById(R.id.editTxtPesquisarCliente);
         clienteDto = new ClienteDTO();
         loadClientes();
-        listViewCliente.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            // TODO: 04/04/2017 Debuggar a seleção de item;
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-                    {
-                        clienteSelecionado = new Cliente();
-                        TextView itemSelecionado = (TextView) view.findViewById(R.id.txtIdLista);
-                        clienteSelecionado.setIdCliente(Integer.parseInt(itemSelecionado.getText().toString()));
-
-                        for (Cliente c : clienteDto.getLista())
-                        {
-                            if (c.getIdCliente() == clienteSelecionado.getIdCliente())
-                            {
-                                clienteSelecionado = c;
-                                Toast.makeText(SelecionarClienteActivity.this, clienteSelecionado.getNome() + " " + clienteSelecionado.getSobrenome(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
-                    }
-
-                }
-        );
     }
 
 
+    private void loadClientes() {
+        new DownloadFromWS().execute();
+    }
 
-        private void loadClientes ()
-        {
-            new DownloadFromWS().execute();
-        }
-
-    public void pesquisarCliente(View v)
-    {
+    public void pesquisarCliente(View v) {
         String s = txtPesquisa.getText().toString().toLowerCase().trim();
-        if (!s.isEmpty())
-        {
+        if (!s.isEmpty()) {
             List<Cliente> listaFiltrada = new ArrayList<Cliente>();
-            if (listaCliente == null)
-            {
+            if (listaCliente == null) {
                 loadClientes();
             }
             for (Cliente cliente : listaCliente
-                    )
-            {
-                if (cliente.getNome().toLowerCase().contains(s) || cliente.getSobrenome().toLowerCase().contains(s) || cliente.getCpf().contains(s))
-                {
+                    ) {
+                if (cliente.getNome().toLowerCase().contains(s) || cliente.getSobrenome().toLowerCase().contains(s) || cliente.getCpf().contains(s)) {
                     listaFiltrada.add(cliente);
                 }
             }
-            if (listaFiltrada.size() != 0)
-            {
+            if (listaFiltrada.size() != 0) {
                 adicionarNaLista(listaFiltrada);
             }
-        } else
-        {
+        } else {
             adicionarNaLista(listaCliente);
         }
     }
 
-    public void adicionarNaLista(List<Cliente> clientes)
-    {
+    public void adicionarNaLista(List<Cliente> clientes) {
         clienteAdapter = new ClienteAdapter(SelecionarClienteActivity.this, R.layout.lista_produto, clientes);
         listViewCliente.setAdapter(clienteAdapter);
     }
 
-    private class DownloadFromWS extends AsyncTask<Void, Void, String>
-    {
+  private class DownloadFromWS extends AsyncTask<Void, Void, String> {
         @Override
-        protected String doInBackground(Void... params)
-        {
+        protected String doInBackground(Void... params) {
             String resultado = null;
 
-            try
-            {
+            try {
                 URL url = new URL("http://10.0.2.2:9999/AlugueServiceWS/WS/Cliente/Pesquisar");
                 resultado = Json.conexaoJsonGet(url);
 
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return resultado;
         }
 
-        protected void onPostExecute(String s)
-        {
+        protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            try
-            {
+            try {
                 clienteDto = Json.convertJSONtoClienteDtoLista(s);
-                if (clienteDto.isOk())
-                {
+                if (clienteDto.isOk()) {
                     adicionarNaLista(clienteDto.getLista());
                     listaCliente = clienteDto.getLista();
                 }
-            } catch (JSONException e)
-            {
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
 
