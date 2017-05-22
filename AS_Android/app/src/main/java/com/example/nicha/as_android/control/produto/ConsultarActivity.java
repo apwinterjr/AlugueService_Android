0a126ddebe196704895abcbddcac4c8f67b9f581
Nichals <nichalsf@hotmail.com>
2017-05-22 14:07:29 -0300

    Adicionado logica para a consulta de produto e compartilhar produto.

--- AS_Android/app/src/main/java/com/example/nicha/as_android/control/produto/ConsultarActivity.java
+++ AS_Android/app/src/main/java/com/example/nicha/as_android/control/produto/ConsultarActivity.java
@@ -1,15 +1,112 @@
 package com.example.nicha.as_android.control.produto;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.nicha.as_android.R;
+import com.example.nicha.as_android.control.pre_aluguel.SelecionarProdutoActivity;
+import com.example.nicha.as_android.dto.ProdutoDTO;
+import com.example.nicha.as_android.model.Produto;
+import com.example.nicha.as_android.util.Json;
+import com.example.nicha.as_android.util.ProdutoAdapter;
 
-public class ConsultarActivity extends Activity {
+import org.json.JSONException;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultarActivity extends Activity
+{
+
+    EditText txtIdProduto;
+    List<Produto> produtos;
+    static Produto produtoSelecionado;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto_consultar);
+        new DownloadFromWS().execute();
+        txtIdProduto = (EditText) findViewById(R.id.txtProdutoConsultar);
+
+    }
+
+
+    public void consultar(View v)
+    {
+        if (txtIdProduto.length() > 0)
+        {
+            for (Produto p : produtos)
+            {
+                if (p.getIdProduto() == Integer.parseInt(txtIdProduto.getText().toString())){
+                    produtoSelecionado = p;
+                }
+            }
+            if (produtoSelecionado.getIdProduto() != 0)
+            {
+                Intent intent = new Intent(v.getContext(), ResultadoActivity.class);
+                startActivity(intent);
+            }else {
+                Toast.makeText(this, "Nenhuma produto encontrado.", Toast.LENGTH_SHORT).show();
+            }
+        } else
+        {
+            Toast.makeText(this, "Favor informar o id do produto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class DownloadFromWS extends AsyncTask<Void, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            String resultado = null;
+
+            try
+            {
+                URL url = new URL(com.example.nicha.as_android.util.Utilitario.URL_WS + "Produto/Pesquisar");
+                resultado = Json.conexaoJsonGet(url);
+
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            return resultado;
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            super.onPostExecute(s);
+            try
+            {
+                ProdutoDTO produtoDto = Json.convertJSONtoProdutoDTOLista(s);
+                if (produtoDto.isOk())
+                {
+                    produtos = new ArrayList<Produto>();
+                    produtos = produtoDto.getLista();
+
+                } else
+                {
+                    Toast.makeText(ConsultarActivity.this, produtoDto.getMensagem(), Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
--- AS_Android/app/src/main/java/com/example/nicha/as_android/control/produto/ResultadoActivity.java
+++ AS_Android/app/src/main/java/com/example/nicha/as_android/control/produto/ResultadoActivity.java
@@ -1,15 +1,75 @@
 package com.example.nicha.as_android.control.produto;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.nicha.as_android.R;
 
-public class ResultadoActivity extends Activity {
+public class ResultadoActivity extends Activity
+{
+
+    TextView textIDConsultarProduto;
+    TextView textNomeConsultarProduto;
+    TextView txtPrecoConsultarProduto;
+    TextView textTamanhoConsultarProduto;
+    TextView textDescricaoConsultarProduto;
+    TextView txtStatusConsultarProduto;
+    Button compartilharProduto;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto_consulta_resultado);
+        textIDConsultarProduto = (TextView) findViewById(R.id.textIDConsultarProduto);
+        textNomeConsultarProduto = (TextView) findViewById(R.id.textNomeConsultarProduto);
+        txtPrecoConsultarProduto = (TextView) findViewById(R.id.txtPrecoConsultarProduto);
+        textTamanhoConsultarProduto = (TextView) findViewById(R.id.textTamanhoConsultarProduto);
+        textDescricaoConsultarProduto = (TextView) findViewById(R.id.textDescricaoConsultarProduto);
+        txtStatusConsultarProduto = (TextView) findViewById(R.id.txtStatusConsultarProduto);
+        compartilharProduto = (Button) findViewById(R.id.btnCompartilharProduto);
+        load();
+    }
+
+    public void compartilhar(View v)
+    {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "[" + ConsultarActivity.produtoSelecionado.getIdProduto() + "] " + ConsultarActivity.produtoSelecionado.getNome() + " - R$" + ConsultarActivity.produtoSelecionado.getValor() + ".");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+    private void load()
+    {
+        textIDConsultarProduto.setText("ID: " + ConsultarActivity.produtoSelecionado.getIdProduto());
+        textNomeConsultarProduto.setText("Nome: " + ConsultarActivity.produtoSelecionado.getNome());
+        txtPrecoConsultarProduto.setText("Preço: " + ConsultarActivity.produtoSelecionado.getValor());
+        textTamanhoConsultarProduto.setText("Tamanho: " + ConsultarActivity.produtoSelecionado.getTamanho());
+        textDescricaoConsultarProduto.setText("Descrição: " + ConsultarActivity.produtoSelecionado.getDescricao());
+        switch (ConsultarActivity.produtoSelecionado.getStatus())
+        {
+            case 0:
+                txtStatusConsultarProduto.setText("Status: " + "Inativo");
+                break;
+            case 1:
+                txtStatusConsultarProduto.setText("Status: " + "Ativo");
+                break;
+            case 2:
+                txtStatusConsultarProduto.setText("Status: " + "Alugado");
+                break;
+            case 3:
+                txtStatusConsultarProduto.setText("Status: " + "Pré-alugado");
+                break;
+            case 4:
+                txtStatusConsultarProduto.setText("Status: " + "Manutenção");
+                break;
+            case 5:
+                txtStatusConsultarProduto.setText("Status: " + "Lavagem");
+        }
     }
 }
